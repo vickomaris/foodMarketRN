fe78494bdea98370b4b42fa57816789d443a9ca7
vickomaris <vicko.maris.s@gmail.com>
2021-06-02 19:12:25 +0700

    menambahkan custom bottom tab navigator: Home, Order, Profile

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,32 @@
 import React, {useEffect} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
 import {
+  Home,
+  Order,
+  Profile,
   SignIn,
   SignUp,
   SignUpAddress,
   SplashScreen,
   SuccessSignUp,
 } from '../pages';
+import {BottomNavigator} from '../components';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainApp = () => {
+  return (
+    <Tab.Navigator tabBar={props => <BottomNavigator {...props} />}>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Order" component={Order} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  );
+};
+
 const Router = () => {
   return (
     <Stack.Navigator>
@@ -35,6 +53,11 @@ const Router = () => {
       <Stack.Screen
         name="SuccessSignUp"
         component={SuccessSignUp}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="MainApp"
+        component={MainApp}
         options={{headerShown: false}}
       />
     </Stack.Navigator>
